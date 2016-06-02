8fe69f1d43f0c9970e38ddf95f74ae548f4d5e5d
Alirio Mendes <alirio_mendes@live.com.pt>
2016-06-03 00:14:00 +0100

    Initial Commit

---
+++ OLX Alirio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OLX Alirio
+//
+//  Created by Alírio Mendes on 03/06/16.
+//  Copyright © 2016 Alírio Mendes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
